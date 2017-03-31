f397b6d01b308d84fb2c5efbe5ac013d026bead3
shineINMore <tanyichen23@163.com>
2017-03-31 14:33:52 +0800

    Create ex9.py

---
+++ Learn PYTHON the HARD WAY/ex9.py
@@ -0,0 +1,16 @@
+# Here's some new strange stuff ,remember tyoe it exactly.
+
+days="Mon Tue Wed Thu Fri Sat Sun"
+months="Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print "Here are the days:",days
+print "Here are the months:\n",months
+
+print """
+
+There's something going on here.
+With the three double-quotes.
+We'll be able to type sa much as we like.
+Even 4 lines if we want,or 5,or 6.
+
+"""
